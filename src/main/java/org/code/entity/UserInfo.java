a3d750f78b87fbe62bfd20f7b888900ca775ac40
Hordur Thor Jonsson <htjonsson@gmail.com>
2024-09-22 20:17:50 +0100

    init - jwt

--- src/main/java/org/code/entity/UserInfo.java
+++ src/main/java/org/code/entity/UserInfo.java
@@ -4,22 +4,20 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
-@Builder
-public class Department {
+@NoArgsConstructor
+public class UserInfo {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long departmentId;
-    private String departmentName;
-    private String departmentAddress;
-    private String departmentCode;
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String name;
+    private String email;
+    private String password;
+    private String roles;
 }
---
+++ src/main/java/org/code/repository/UserInfoRepository.java
@@ -0,0 +1,12 @@
+package org.code.repository;
+
+import org.code.entity.UserInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UserInfoRepository extends JpaRepository<UserInfo, Integer> {
+    Optional<UserInfo> findByEmail(String email);
+}
